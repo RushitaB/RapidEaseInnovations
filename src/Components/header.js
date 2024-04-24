97ef7df78f1b668f68f5351e89b394144fb74dc3
Rushita Bhakhar <r.bhakar@clerkswell.com>
2024-04-24 21:53:34 +0530

    first commit

---
+++ src/Components/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="masthead">
+        <div className="container">
+        <div className="masthead-heading text-uppercase">POS</div>
+            <div className="masthead-subheading">Empower your business with our Point of Sale solution</div>
+            <a className="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
+        </div>
+    </header>
+  )
+}
+
+export default Header
